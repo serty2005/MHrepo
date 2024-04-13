52751893bd0a125d0030d0efba0e6bd3f42280d3
serty <serty2005@gmail.com>
2024-04-14 01:14:26 +0300

    Начал писать модуль сравнения entities в двух базах по SN и дате окончания. Нарисовал код для отправки, но пока не тестил. В консоль отдаёт верно.

---
+++ pushchangestosd.py
@@ -0,0 +1,49 @@
+import sqlite3
+import requests
+from dateutil import parser
+
+
+# Функция для сравнения данных и отправки запроса на редактирование объекта в SD
+def compare_and_update():
+    conn_json = sqlite3.connect('fiscal_registers_fromPOS.db')
+    conn_sd = sqlite3.connect('fiscal_registers_fromSD.db')
+    c_json = conn_json.cursor()
+    c_sd = conn_sd.cursor()
+
+    # Выбираем данные для сравнения из базы данных SD
+    c_sd.execute('''SELECT FRSerialNumber, FNNumber, FNExpireDate, UUID
+                    FROM fiscal_registers''')
+    sd_data = c_sd.fetchall()
+
+    # Выбираем данные для сравнения из базы данных JSON
+    c_json.execute('''SELECT serialNumber, fn_serial, dateTime_end
+                      FROM fiscal_registers''')
+    json_data = c_json.fetchall()
+
+    # Сравниваем данные и отправляем запросы на обновление при несоответствии
+    for sd_entry in sd_data:
+        for json_entry in json_data:
+            # Проверяем совпадение по FRSerialNumber и FNNumber
+            if sd_entry[0] == json_entry[0]:
+                # Преобразуем даты из строкового формата в объекты datetime для корректного сравнения
+                sd_date = parser.parse(sd_entry[2])
+                json_date = parser.parse(json_entry[2])
+
+                if sd_date != json_date:  # Сравниваем даты
+                    # Выводим UUID объекта для тестирования
+                    print(f"Объект с UUID {sd_entry[3]} будет изменен.")
+                    # # Отправляем запрос на редактирование объекта в SD
+                    # edit_url = f'https://myhoreca.itsm365.com/sd/services/rest/edit/{sd_entry[3]}/'
+                    # params = {'accessKey': 'ваш_ключ_доступа', 'FNExpireDate': json_entry[2]}
+                    # response = requests.post(edit_url, params=params)
+                    # if response.status_code == 200:
+                    #     print(f"Объект с UUID {sd_entry[3]} успешно обновлен.")
+                    # else:
+                    #     print(f"Ошибка при обновлении объекта с UUID {sd_entry[3]}:", response.status_code)
+
+    conn_json.close()
+    conn_sd.close()
+
+
+# Вызываем функцию для сравнения данных и обновления объектов при несоответствии
+compare_and_update()
